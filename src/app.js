54134c7c36a31bdd3bb05d5878926e4648069e2f
JDASL <127839733+JDASL@users.noreply.github.com>
2023-08-07 22:02:28 +0200

    added form and search

--- src/app.js
+++ src/app.js
@@ -43,7 +43,17 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "6ef2870404e66ed2d6d7e6a9e918298b";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Oslo&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "6ef2870404e66ed2d6d7e6a9e918298b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
